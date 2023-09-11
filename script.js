b95951adaf8b086a53965b050f3e16074f7220f3
FreddoBianca <bvfreddo@fhcs.unp.edu.ar>
2023-09-11 13:02:54 -0300

    agregue js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+alert("Bienvenidxs al sitio web Sustentabilidad Ambiental");
+
+function nombre() {
+    let nombre = prompt("¿Cómo te llamas?");
+  
+    if (nombre !== null) {
+      console.log("Tu nombre es: " + nombre);
+    } else {
+      console.log("El usuario canceló la operación.");
+    }
+  }
+  
+  nombre();

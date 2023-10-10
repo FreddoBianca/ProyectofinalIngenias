05c5e7db87a3c41f789eb8f51c291ff01d9b8a77
FreddoBianca <bvfreddo@fhcs.unp.edu.ar>
2023-10-09 21:46:18 -0300

    boton carrito arreglado

--- js/iniciativas.js
+++ js/iniciativas.js
@@ -1,30 +1,31 @@
 let proyectos = `[
     {
       "proyecto": "Tu huella ecologica es tu impronta",
-      "autora": "luisa",
+      "autora": "Luisa",
       "contacto": "12345678",
       "imagen": "https://st3.depositphotos.com/17418852/32153/v/450/depositphotos_321539600-stock-illustration-trendy-young-woman-presenting-hand.jpg"
     },
     {
       "proyecto": "Esperamos el impacto",
-      "autora": "vanesa",
+      "autora": "Vanesa",
       "contacto": "12345678",
       "imagen": "https://media.istockphoto.com/id/1001310714/es/vector/mujer-se%C3%B1alando-con.jpg?s=170667a&w=0&k=20&c=ehqZXFKCWkVQ06h0HaeJXUIqVAjFbsWD2agn3FJbzGM="
     },
     {
       "proyecto": "Nos importa el ambiente",
-      "autora": "lucia",
+      "autora": "Lucia",
       "contacto": "12345678",
       "imagen": "https://media.istockphoto.com/id/1362447090/es/vector/una-maestra-que-recibe-ideas-de-pie-con-un-gesto-de-la-mano-que-se%C3%B1ala.jpg?s=612x612&w=0&k=20&c=ehd-Yj_46QTkYSHC-e1CA8HJmZEBZzFstax8himEluU="
     },
     {
       "proyecto": "El planeta nos necesita",
-      "autora": "carmen",
+      "autora": "Carmen",
       "contacto": "12345678",
       "imagen": "https://us.123rf.com/450wm/kuklamalvina/kuklamalvina2302/kuklamalvina230200048/199288950-woman-writing-in-notebook-journalist-author-student-business-woman-taking-notes-with-pen-education.jpg?ver=6"
     }
 ]`
 
+window.onload = () => {
 let jsonConvertido = JSON.parse (proyectos)
 console.log(jsonConvertido);
 
@@ -48,6 +49,7 @@ for (let i = 0; i < jsonConvertido.length; i++) {
 
     // crear boton
     let boton = document.createElement("button")
+    boton.className = "botoncarrito"
     boton.innerText = "Agregar al carrito"
 
     //terminar de armar el article
@@ -58,28 +60,50 @@ for (let i = 0; i < jsonConvertido.length; i++) {
 
      //mandar el article a la seccion
      document.getElementById("Proyectos").appendChild(article)
+     
     }
 
+ 
+    // window.onload = () => {
+      let botones = document.querySelectorAll(".botoncarrito");
+      for (let i = 0; i < botones.length; i++)
+          botones[i].addEventListener("click", () => {
+              localStorage.setItem("proyectoProyecto", jsonConvertido[i].proyecto)
+              localStorage.setItem("proyectoAutora", jsonConvertido[i].autora)
+              localStorage.setItem("proyectoContacto", jsonConvertido[i].contacto)
+              localStorage.setItem("proyectoImagen", jsonConvertido[i].imagen)
 
-    window.onload = () => {
-      let botones = document.querySelectorAll("button");
-      for (let i =0; i<botones.lenght;i++){
-        botones[i].addEventListener("click", (event)=>{
-          event.preventDefault()
-          localStorage.setItem("proyectoProyecto", jsonConvertido[i].proyecto)
-          localStorage.setItem("proyectoAutora", jsonConvertido[i].autora)
-          localStorage.setItem("proyectoImagen", jsonConvertido[i].imagen)
+                           
+              let article2 = document.createElement("article2")
   
-          let article = document.createElement("article")
-          //crear h2
-          let h2 = document.createElement("h2")
-          // h2.innerText = jsonConvertido[0].titulo
-          h2.innerText = localStorage.getItem("proyectoP")
-          article.appendChild(h2)
-          document.getElementById("carrito").appendChild(article)
-      })
-  }
+              // Crear h2
+              let h2 = document.createElement("h2")
+              h2.innerText = localStorage.getItem("proyectoProyecto")
+              article2.appendChild(h2);
   
+              // Crear p
+              let p = document.createElement("p")
+              p.innerText = localStorage.getItem("proyectoAutora")
+              article2.appendChild(p)
   
+              // Crear img
+              // let img = document.createElement("img");
+              // img.src = localStorage.getItem("proyectosImagen");
+              // img.style.width = "40%";
+              // article2.appendChild(img);
   
-  }
+              document.getElementById("Carrito").appendChild(article2)
+          });
+      };
+
+  
+
+
+
+
+
+
+
+
+
+  
